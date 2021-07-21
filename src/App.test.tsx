df4e07611d242740d9f35459db0c4fa1e27049c1
IlyaBelkov <Softgrad2020>
2021-07-21 19:11:55 +0300

    fix: titles for tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,10 +5,13 @@ import { store } from './store/store'
 import { App } from './App'
 import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<Provider store={store}><App /></Provider>)
-  const changeTheme = screen.getByLabelText(/dark mode checkbox/)
-  expect(screen.getByRole('main') ).toHaveStyle('background: #fff')
-  userEvent.click(changeTheme)
-  expect(screen.getByRole('main') ).toHaveStyle('background: #000')
+describe('dark mode theme', () => {
+  it('should change the theme', function () {
+    render(<Provider store={store}><App /></Provider>)
+    const changeTheme = screen.getByLabelText(/dark mode checkbox/)
+    expect(screen.getByRole('main') ).toHaveStyle('background: #fff')
+    userEvent.click(changeTheme)
+    expect(screen.getByRole('main') ).toHaveStyle('background: #000')
+  })
+
 })
--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from '../../store/store'
 
 describe('when rendered', () => {
-  it('should be a button to change the theme', () => {
+  it('should be a button which change the theme', () => {
     render(<Provider store={store}><Header /></Provider>)
     const buttonChangeTheme = screen.getByLabelText(/dark mode checkbox/)
     expect(buttonChangeTheme).toBeInTheDocument()
